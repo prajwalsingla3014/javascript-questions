a37fe77ccd72e75fac2ecd74c9b0dfee133bd0d0
Prajwal Singla <singlaprajwal.99@gmail.com>
2025-04-18 23:29:11 +0530

    feat: Added polyfill for javascript apply method

--- applyMethodPolyfill.js
+++ applyMethodPolyfill.js
@@ -1,31 +1,33 @@
-
 const car = {
-    color: 'Black',
-    model: 'BMW'
-}
+  color: "Black",
+  model: "BMW",
+};
 
-function purchaseCar(currency,price) {
-    console.log(`I have purchased ${this.color} - ${this.model} car for ${currency}${price}`)
+function purchaseCar(currency, price) {
+  console.log(
+    `I have purchased ${this.color} - ${this.model} car for ${currency}${price}`
+  );
 }
 
 // General method which JS provides
-purchaseCar.apply(car,['Rs',7000000])
+purchaseCar.apply(car, ["Rs", 7000000]);
 
 // Polyfill for Apply Method
-Function.prototype.myApply = function(context,args) {
-    if(typeof this !== 'function') {
-        throw new TypeError('myApply must be called on a function')
-    }
+Function.prototype.myApply = function (context, args) {
+  if (typeof this !== "function") {
+    throw new TypeError("myApply must be called on a function");
+  }
 
-    context = context !== undefined && context !== null ? Object(context) : globalThis
-    if(!Array.isArray(args)) {
-        throw new TypeError('The second argument must be an array')
-    }
-    const uniqueSymbol = Symbol()
-    context[uniqueSymbol] = this
-    const result = context[uniqueSymbol](...args)
-    delete context[uniqueSymbol]
-    return result
-}
+  context =
+    context !== undefined && context !== null ? Object(context) : globalThis;
+  if (!Array.isArray(args)) {
+    throw new TypeError("The second argument must be an array");
+  }
+  const uniqueSymbol = Symbol();
+  context[uniqueSymbol] = this;
+  const result = context[uniqueSymbol](...args);
+  delete context[uniqueSymbol];
+  return result;
+};
 
-purchaseCar.myApply(car,['Rs', 6000000])
+purchaseCar.myApply(car, ["Rs", 6000000]);
