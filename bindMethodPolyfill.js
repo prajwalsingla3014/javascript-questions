c4e53588799927937554bb52464a3436fa6b9ab8
Prajwal Singla <singlaprajwal.99@gmail.com>
2025-04-18 23:30:08 +0530

    feat: Added polyfill for javascript bind method

--- bindMethodPolyfill.js
+++ bindMethodPolyfill.js
@@ -4,7 +4,6 @@
     the function but returns a new function that can be invoked later.
 */
 
-
 const car = {
   color: "Black",
   company: "BMW",
@@ -17,33 +16,33 @@ function purchaseCar(currency, price) {
 }
 
 // Bind Method which is provided by JS
-const newFunc = purchaseCar.bind(car,'Rs',7000000)
-newFunc()
+const newFunc = purchaseCar.bind(car, "Rs", 7000000);
+newFunc();
 
 // The returned function can also take arguments
-const newFunc1 = purchaseCar.bind(car,'Rs')
-newFunc1(7500000)
+const newFunc1 = purchaseCar.bind(car, "Rs");
+newFunc1(7500000);
 
 // Polyfill for Bind Method
 
-Function.prototype.myBind = function(context,...args) {
-    if(typeof this !== 'function') {
-        throw new TypeError('myBind must be called on a function')
-    }
+Function.prototype.myBind = function (context, ...args) {
+  if (typeof this !== "function") {
+    throw new TypeError("myBind must be called on a function");
+  }
 
-    context = context !== undefined && context !== null ? context : globalThis
-    const uniqueSymbol = Symbol()
-    context[uniqueSymbol] = this
-    return function(...newArgs) {
-        const result = context[uniqueSymbol](...args,...newArgs);
-        delete context[uniqueSymbol]
-        return result;
-    }
-}
+  context = context !== undefined && context !== null ? context : globalThis;
+  const uniqueSymbol = Symbol();
+  context[uniqueSymbol] = this;
+  return function (...newArgs) {
+    const result = context[uniqueSymbol](...args, ...newArgs);
+    delete context[uniqueSymbol];
+    return result;
+  };
+};
 
 // One way of using myBind
-const newFunc2 = purchaseCar.myBind(car,'Rs',8000000)
-newFunc2()
+const newFunc2 = purchaseCar.myBind(car, "Rs", 8000000);
+newFunc2();
 
 // Other Way of using myBind with passinf arguments in returned function
 const newFunc3 = purchaseCar.myBind(car, "Rs");
