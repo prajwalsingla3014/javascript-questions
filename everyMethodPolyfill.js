9b601165e8dbe539752a3d44cd2af32d7d1401dd
Prajwal Singla <singlaprajwal.99@gmail.com>
2025-04-18 23:32:46 +0530

    feat: Added polyfill for every method of Javascript

--- everyMethodPolyfill.js
+++ everyMethodPolyfill.js
@@ -1,22 +1,22 @@
-const arr = [1,2,3,4,5]
+const arr = [1, 2, 3, 4, 5];
 
-const allPositive = arr.every(x => x > 0)
-console.log(allPositive)
+const allPositive = arr.every((x) => x > 0);
+console.log(allPositive);
 
 // Polyfill for Every Method
 
-Array.prototype.myEvery = function(callback) {
-    if(typeof callback !== 'function') {
-        throw new TypeError(callback + ' is not a function')
-    }
+Array.prototype.myEvery = function (callback) {
+  if (typeof callback !== "function") {
+    throw new TypeError(callback + " is not a function");
+  }
 
-    for(let i=0;i<this.length;i++) {
-        if(!callback(this[i],i,this)) {
-            return false
-        }
+  for (let i = 0; i < this.length; i++) {
+    if (!callback(this[i], i, this)) {
+      return false;
     }
-    return true
-}
+  }
+  return true;
+};
 
-const allPositiveNumbers = arr.myEvery(x => x>0)
-console.log('All positive numbers', allPositiveNumbers)
+const allPositiveNumbers = arr.myEvery((x) => x > 0);
+console.log("All positive numbers", allPositiveNumbers);
