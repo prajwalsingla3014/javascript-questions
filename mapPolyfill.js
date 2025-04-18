b820cac07e3202ab00cc13536618b5e88fe5ad2a
Prajwal Singla <singlaprajwal.99@gmail.com>
2025-04-18 23:35:24 +0530

    feat: Added polyfill for map method of Javascript

--- mapPolyfill.js
+++ mapPolyfill.js
@@ -1,20 +1,20 @@
-let arr = [1,2,3,4,5]
+let arr = [1, 2, 3, 4, 5];
 
-const mappedArr = arr.map(a => a*2);
-console.log(mappedArr)
+const mappedArr = arr.map((a) => a * 2);
+console.log(mappedArr);
 
 // Polyfill for Map function
 
-Array.prototype.myMap = function(callback) {
-    if(typeof callback !== 'function') {
-        throw new TypeError(callback + ' is not a function')
-    }
-    let result = []
-    for(let i=0;i<this.length;i++) {
-        result.push(callback(this[i],i,this))
-    }
-    return result;
-}
+Array.prototype.myMap = function (callback) {
+  if (typeof callback !== "function") {
+    throw new TypeError(callback + " is not a function");
+  }
+  let result = [];
+  for (let i = 0; i < this.length; i++) {
+    result.push(callback(this[i], i, this));
+  }
+  return result;
+};
 
-const mappedArray = arr.myMap(a => a*3)
-console.log('mapped Array ',mappedArray)
+const mappedArray = arr.myMap((a) => a * 3);
+console.log("mapped Array ", mappedArray);
