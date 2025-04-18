d11cab23b37c1acf6f04758b4f8a7ff6a132febb
Prajwal Singla <singlaprajwal.99@gmail.com>
2025-04-18 23:35:01 +0530

    feat: Added polyfill for flatten object

--- flatNestedObject.js
+++ flatNestedObject.js
@@ -5,22 +5,22 @@ const data = {
     city: "Jalandhar",
     pincode: 144005,
   },
-  hobbies: ['Travelling',"Music"]
+  hobbies: ["Travelling", "Music"],
 };
 
 // Polyfill for flat nested object with notation _
 
-function flattenObj(obj = {},parent = '',result = {}) {
-    for(let key in obj) {
-        let propName = parent ? parent + '_' + key : key;
-        if(typeof obj[key] === 'object') {
-            flattenObj(obj[key],propName,result)
-        } else {
-            result[propName] = obj[key]
-        }
+function flattenObj(obj = {}, parent = "", result = {}) {
+  for (let key in obj) {
+    let propName = parent ? parent + "_" + key : key;
+    if (typeof obj[key] === "object") {
+      flattenObj(obj[key], propName, result);
+    } else {
+      result[propName] = obj[key];
     }
-    return result;
+  }
+  return result;
 }
 
-const flatObj  = flattenObj(data)
-console.log('Flat Obj ', flatObj)
+const flatObj = flattenObj(data);
+console.log("Flat Obj ", flatObj);
