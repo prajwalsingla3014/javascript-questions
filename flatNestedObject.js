800c37cb70b3a55f9fee022878e4cad990a44f54
Prajwal Singla <singlaprajwal.99@gmail.com>
2025-04-18 22:41:31 +0530

    feat: Added polyfill for flat method for object

---
+++ flatNestedObject.js
@@ -0,0 +1,26 @@
+const data = {
+  name: "Prajwal",
+  age: 26,
+  address: {
+    city: "Jalandhar",
+    pincode: 144005,
+  },
+  hobbies: ['Travelling',"Music"]
+};
+
+// Polyfill for flat nested object with notation _
+
+function flattenObj(obj = {},parent = '',result = {}) {
+    for(let key in obj) {
+        let propName = parent ? parent + '_' + key : key;
+        if(typeof obj[key] === 'object') {
+            flattenObj(obj[key],propName,result)
+        } else {
+            result[propName] = obj[key]
+        }
+    }
+    return result;
+}
+
+const flatObj  = flattenObj(data)
+console.log('Flat Obj ', flatObj)
