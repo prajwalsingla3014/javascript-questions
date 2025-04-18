4426c63e22ec75dbfe923cf56c001f5c3742951f
Prajwal Singla <singlaprajwal.99@gmail.com>
2025-04-18 23:30:31 +0530

    feat: Added polyfill for javascript call method

--- callMethodPolyfill.js
+++ callMethodPolyfill.js
@@ -4,32 +4,34 @@
     the context object as the first argument followed by individual arguments.
 */
 
-
 const car = {
-    color: 'Black',
-    model: 'BMW'
-}
+  color: "Black",
+  model: "BMW",
+};
 
-function purchaseCar(currency,price) {
-    console.log(`I have purchased ${this.color} - ${this.model} car for ${currency}${price}`)
+function purchaseCar(currency, price) {
+  console.log(
+    `I have purchased ${this.color} - ${this.model} car for ${currency}${price}`
+  );
 }
 
 // Call Method Provided by JS
-purchaseCar.call(car,'Rs',7000000)
+purchaseCar.call(car, "Rs", 7000000);
 
 // Polyfill for Call Method
 
-Function.prototype.myCall = function(context,...args) {
-    if(typeof this !== 'function') {
-        throw new TypeError('myCall must be called on a function')
-    }
+Function.prototype.myCall = function (context, ...args) {
+  if (typeof this !== "function") {
+    throw new TypeError("myCall must be called on a function");
+  }
 
-    context = context !== undefined && context !== null ? Object(context) : globalThis
-    const uniqueSymbol = Symbol()
-    context[uniqueSymbol] = this;
-    const result = context[uniqueSymbol](...args)
-    delete context[uniqueSymbol]
-    return result
-}
+  context =
+    context !== undefined && context !== null ? Object(context) : globalThis;
+  const uniqueSymbol = Symbol();
+  context[uniqueSymbol] = this;
+  const result = context[uniqueSymbol](...args);
+  delete context[uniqueSymbol];
+  return result;
+};
 
-purchaseCar.myCall(car,'Rs',6000000)
+purchaseCar.myCall(car, "Rs", 6000000);
