cbaba924d32fa52b80623987f5200befda7b66a6
Prajwal Singla <singlaprajwal.99@gmail.com>
2025-04-18 23:19:46 +0530

    feat: Added polyfill for Promise.all

--- promiseAllPolyfill.js
+++ promiseAllPolyfill.js
@@ -5,30 +5,42 @@
     fulfilled it returns array of fulfilled promises
 */
 
-
-const promiseVal = Promise.all([Promise.resolve(1),new Promise(resolve => setTimeout(() => resolve(2),1000)),Promise.reject(3)])
-promiseVal.then(data => console.log('Resolved with ', data)).catch(err => console.log('Rejected with ', err))
+const promiseVal = Promise.all([
+  Promise.resolve(1),
+  new Promise((resolve) => setTimeout(() => resolve(2), 1000)),
+  Promise.reject(3),
+]);
+promiseVal
+  .then((data) => console.log("Resolved with ", data))
+  .catch((err) => console.log("Rejected with ", err));
 
 // Polyfill for Promise.all
 
-Promise.myAll = function(promiseArr) {
-    const result = []
-    let totalResolved = 0;
-    return new Promise((resolve,reject) => {
-        promiseArr.forEach((promise,index) => {
-            Promise.resolve(promise).then(data => {
-                result[index] = data
-                totalResolved++
-                if(totalResolved === promiseArr.length) {
-                    resolve(result)
-                }
-            }).catch(error => {
-                reject(error)
-            })
+Promise.myAll = function (promiseArr) {
+  const result = [];
+  let totalResolved = 0;
+  return new Promise((resolve, reject) => {
+    promiseArr.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then((data) => {
+          result[index] = data;
+          totalResolved++;
+          if (totalResolved === promiseArr.length) {
+            resolve(result);
+          }
         })
-    })
-}
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  });
+};
 
-const promiseValue = Promise.myAll([Promise.resolve(1),new Promise(resolve => setTimeout(() => resolve(2),1000)),Promise.resolve(3)])
-promiseValue.then(value => console.log('Resolved with ', value)).catch(err => console.log('Rejected with ', err))
-
+const promiseValue = Promise.myAll([
+  Promise.resolve(1),
+  new Promise((resolve) => setTimeout(() => resolve(2), 1000)),
+  Promise.resolve(3),
+]);
+promiseValue
+  .then((value) => console.log("Resolved with ", value))
+  .catch((err) => console.log("Rejected with ", err));
