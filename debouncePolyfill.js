8c41ddb119e4d84ce4e0dbd2bcf2843c3132663a
Prajwal Singla <singlaprajwal.99@gmail.com>
2025-04-18 15:18:15 +0530

    feat: Added polyfill for debounce method from lodash

---
+++ debouncePolyfill.js
@@ -0,0 +1,30 @@
+/*
+    Debouncing is a programming practice used to ensure that time-consuming tasks
+    do not fire so often, that it stalls the performance of the web page.In
+    debouncing,the function is only executed after a specific delay since the last
+    event's occurrence.Debouncing is ideal when you want to wait for a pause in the events
+    before triggering a function.This is useful for situations like search suggestions.
+*/
+
+// PolyFill for debounce function
+
+const myDebounce = (callback,delay) => {
+    let timerId;
+    return function(...args) {
+        if(timerId) {
+            clearTimeout(timerId)
+        }
+        timerId = setTimeout(() => {
+            callback(...args)
+        },delay)
+    }
+}
+
+let startTime = Date.now()
+
+const fetchData = () => {
+    console.log(`fetchData called after ${Date.now() - startTime}ms`)
+}
+
+const debouncedFunc = myDebounce(fetchData,50)
+debouncedFunc()
